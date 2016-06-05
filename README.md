28a270d39127c77ad4d445198620a1f9de6ef53a
Nguyễn Việt Pháp <51202664@hcmut.edu.vn>
2016-06-05 16:07:26 +0700

    12
    1

--- README.md
+++
@@ -1,4 +0,0 @@
-# hello-world
-example
-
-I'm Phap. Hello!!!
--- main.js
+++
@@ -1,5 +0,0 @@
-function printHello(){
-   console.log( "Hello, World!");
-}
-// Now call above function after 2 seconds
-setInterval(printHello, 2000);
